8a3f16e7df4b88e94d347e9682b7a8833cd9a6df
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-19 22:11:38 +0530

    greater number

---
+++ p5_GreatestNumber.java
@@ -0,0 +1,12 @@
+public class p5_GreatestNumber {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 56;
+        if(n1 > n2){
+            System.out.println(n1 + " is greater");
+        }   
+        else{
+            System.err.println(n2 + " is greter");
+        }
+    }
+}
