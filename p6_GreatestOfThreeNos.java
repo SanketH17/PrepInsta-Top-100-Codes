966e69d488eb53a9f1f9bdbe3bf65aeac3fd6d0b
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-19 22:16:47 +0530

    Greatest of three numbers

---
+++ p6_GreatestOfThreeNos.java
@@ -0,0 +1,17 @@
+public class p6_GreatestOfThreeNos {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 58;
+        int n3 = 36;
+
+        if(n1 > n2 && n1 > n3){
+            System.out.println(n1 + " is greater");
+        }
+        else if(n2 > n3){
+            System.out.println(n2 + " is greater");
+        }
+        else{
+            System.out.println(n3 + " is greater");
+        }
+    }
+}
