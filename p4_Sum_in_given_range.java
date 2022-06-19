094e3bd0d67552e9121941b50f938a1271bdd7a2
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-19 22:08:05 +0530

    sum of n in a given range

---
+++ p4_Sum_in_given_range.java
@@ -0,0 +1,11 @@
+public class p4_Sum_in_given_range {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 10;
+        int sum = 0;
+        for(int i = n1; i <= n2; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
