0a95da473e067538f7968fa8e4bb49787252ee06
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-19 22:37:25 +0530

    sum of digits of a number

---
+++ p10_SumOfDigits.java
@@ -0,0 +1,14 @@
+public class p10_SumOfDigits {
+    public static void main(String[] args) {
+        int n = 1234;
+        int sum = 0;
+        int r;
+        while(n > 0){
+            r = n % 10;
+            sum = sum + r;
+            n = n / 10;
+        }
+
+        System.out.println(sum);
+    }   
+}
