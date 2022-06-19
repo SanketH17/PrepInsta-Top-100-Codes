3803bd42cddd13106bdf3d65728863b1269f6a03
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-19 21:57:47 +0530

    Positive or negative

---
+++ p1_CheckNumber.java
@@ -0,0 +1,11 @@
+public class p1_CheckNumber {
+    public static void main(String[] args) {
+        int num = 11;
+        if(num > 0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
