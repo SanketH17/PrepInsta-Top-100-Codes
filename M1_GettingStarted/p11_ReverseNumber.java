bab68f8350445f2b01611dc0db387e2dbd743736
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-21 07:18:00 +0530

    Reverse the number

---
+++ M1_GettingStarted/p11_ReverseNumber.java
@@ -0,0 +1,15 @@
+package M1_GettingStarted;
+
+public class p11_ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12;
+        int r;
+        int res = 0;
+        while(num > 0){
+            r = num % 10;
+            res = res * 10 + r;
+            num = num / 10;
+        }
+        System.out.println(res);
+    }
+}
